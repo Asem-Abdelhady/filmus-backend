939641bae3182d12fe0c3cf62fee40f2d6fb5842
Asem-Abdelhady <asemshawkey@gmail.com>
2023-04-19 04:12:57 +0300

    Add: types for IMDB api calls

--- services/ImdbService.ts
+++ services/ImdbService.ts
@@ -1,8 +1,14 @@
 import axios from 'axios';
 import { ErrorHandler } from '../middleware/ErrorMiddleware';
 import { API_KEY, BASE_URL } from '../utils/config';
+import type IApiWholeResponse from '../types/ApiWholeResponse';
+import type IMovieData from '../types/ApiSingleResponse';
+import type IVideoData from '../types/ApiVideoResponse';
+import type ICreditsData from '../types/ApiCreditReponse';
 
-export async function getTrending(pageNumber: string): Promise<any> {
+export async function getTrending(
+    pageNumber: string
+): Promise<IApiWholeResponse> {
     try {
         let page: number = parseInt(pageNumber);
         if (page === 0 || page > 1000) {
@@ -20,7 +26,9 @@ export async function getTrending(pageNumber: string): Promise<any> {
     }
 }
 
-export async function getImdbMovies(pageNumber: string): Promise<any> {
+export async function getImdbMovies(
+    pageNumber: string
+): Promise<IApiWholeResponse> {
     try {
         let page: number = parseInt(pageNumber);
         if (page === 0 || page > 1000) {
@@ -35,7 +43,7 @@ export async function getImdbMovies(pageNumber: string): Promise<any> {
     }
 }
 
-export async function getMovieDetail(movieId: string): Promise<any> {
+export async function getMovieDetail(movieId: string): Promise<IMovieData> {
     try {
         const id: number = parseInt(movieId);
 
@@ -48,7 +56,7 @@ export async function getMovieDetail(movieId: string): Promise<any> {
     }
 }
 
-export async function getMovieVideos(movieId: string): Promise<any> {
+export async function getMovieVideos(movieId: string): Promise<IVideoData> {
     try {
         const id: number = parseInt(movieId);
 
@@ -61,7 +69,7 @@ export async function getMovieVideos(movieId: string): Promise<any> {
     }
 }
 
-export async function getMovieCredits(movieId: string): Promise<any> {
+export async function getMovieCredits(movieId: string): Promise<ICreditsData> {
     try {
         const id: number = parseInt(movieId);
 
